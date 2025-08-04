fc6ebda213867513d77e8fcd08f27b48bdd8ce52
ZenithDev1 <devendra702321@gmail.com>
2025-08-04 23:28:01 +0530

    Detailed error implimentation and testing done

--- productlist/src/main/java/com/bitsnbytes/product/exception/GlobalExceptionHandler.java
+++ productlist/src/main/java/com/bitsnbytes/product/exception/GlobalExceptionHandler.java
@@ -1,16 +1,27 @@
 package com.bitsnbytes.product.exception;
 
+import com.bitsnbytes.product.dto.ExceptionResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException {
 
     @ExceptionHandler(CategoryAlreadyExistsException.class)
-    public ResponseEntity<String> handleCategoryAlreadyExistsException(CategoryAlreadyExistsException ex){
-        return  ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<ExceptionResponseDTO> handleCategoryAlreadyExistsException(CategoryAlreadyExistsException ex
+    , WebRequest webRequest){
+        ExceptionResponseDTO exceptionResponseDTO = new ExceptionResponseDTO(
+                webRequest.getDescription(false),
+                    HttpStatus.CONFLICT,
+                    ex.getMessage(),
+                    LocalDateTime.now()
+        );
+        return  ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionResponseDTO);
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
